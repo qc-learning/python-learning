67ca489ccc01ff9a39ea6c9c4376fc7478080ee7
qc-310 <ssqc0310@outlook.jp>
2024-07-07 18:11:36 +0900

    complete step2

---
+++ step2.py
@@ -0,0 +1,18 @@
+# 変数の破棄del()
+sample_number = 100
+print(sample_number)
+del(sample_number)
+# print(sample_number)
+
+# 変数の形を調べるtype()
+sample_number1 = 999
+print(type(sample_number1))
+
+# 比較演算子
+x1 = 80; y1 = 60
+print(x1 >= y1)
+
+# 論理演算子
+# 論理積
+x2 = 80
+print(x2 < 100 and x2 > 50)
